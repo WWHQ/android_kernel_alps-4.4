8ebc8309569699eefa69417bda2c6a2d5b79c047
谓我何求 <54165654+WWHQ@users.noreply.github.com>
2026-05-17 10:29:05 +0800

    Update memory.c

--- drivers/rmm/memory.c
+++ drivers/rmm/memory.c
@@ -30,12 +30,13 @@
 
 	#include "memory.h"
 	#include <linux/mm.h>
-	#include <linux/sched.h>
 	#include <linux/slab.h>
 	#include <linux/uaccess.h>
 	#include <linux/version.h>
+	#include <linux/sched.h>
 	#include <linux/sched/task.h>
 	#include <linux/pid.h>
+
 	
 	bool read_process_memory(pid_t pid, uintptr_t addr, void *buffer, size_t size)
 	{
